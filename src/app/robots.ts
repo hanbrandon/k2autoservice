d1207532ff55ab93ec269599e7e5c1156f2fa684
Sang Hyun Han <hanbrandon@hotmail.com>
2026-05-06 10:53:56 -0700

    Allow Facebook crawlers in robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,11 +3,21 @@ import { BUSINESS_INFO } from '@/utils/businessInfo';
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/api/',
-        },
+        rules: [
+            {
+                userAgent: 'facebookexternalhit',
+                allow: '/',
+            },
+            {
+                userAgent: 'Facebot',
+                allow: '/',
+            },
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: '/api/',
+            },
+        ],
         sitemap: `${BUSINESS_INFO.siteUrl}/sitemap.xml`,
     };
 }
